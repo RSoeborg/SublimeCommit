59008274864f2424daff6603634e56202fa70c2e
Rasmus Soeborg <rsoeborg@gmail.com>
2024-08-16 10:45:37 +0200

    chore: update installation script and version update for new release

--- Install.sh
+++ Install.sh
@@ -1,2 +1,5 @@
 dotnet tool uninstall SublimeCommit --global
 dotnet tool install --global --add-source ./nupkg SublimeCommit
+
+# Pushing:
+# dotnet nuget push nupkg/SublimeCommit.1.0.3.nupkg --api-key HERE --source https://api.nuget.org/v3/index.json
